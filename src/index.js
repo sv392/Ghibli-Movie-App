5857fc7c186c7a6eeabdbcb00272cd0b51e80d48
Sanh Vu <sanhvu020@gmail.com>
2022-11-19 18:36:48 -0500

    Added functions to turn movie titles to links and show movie info when clicked

--- src/index.js
+++ src/index.js
@@ -11,9 +11,29 @@ function getMovies() {
     .then(data => {
         const movieList = data.map((movie) => ul.innerHTML += 
         `
-        <li>${movie.title}</li>
+        <li><a href="#" data-id="${movie.id}">${movie.title}</a></li>
         `
         );
         return movieList
+    })
+    attachClicksToLinks()
+}
+
+const attachClicksToLinks =  () => {
+    const movies = document.querySelectorAll('a')
+    movies.forEach((movie) => {
+        movie.addEventListener('click', displayMovie)
+    })
+}
+
+const displayMovie = (event) => {
+    console.log(event.target.dataset.id)
+    const info = document.getElementById('info')
+    const ul = document.getElementById("movie-list")
+    ul.innerHTML = ''
+    fetch(BASE_URL + `/shows/${event.target.dataset.id}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
     })
 }
